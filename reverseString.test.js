1f6ef464517831f476e502390e0eabeb0185b01a
J-724 <jjpernian@gmail.com>
2022-07-07 21:30:44 -0400

    reverseString done

---
+++ reverseString.test.js
@@ -0,0 +1,19 @@
+import reverseString from './reverseString';
+
+describe('reverseString', () => {
+    test('single word string', () => {
+        expect(reverseString('hello')).toBe('olleh');
+    });
+
+    test('multiple words string', () => {
+        expect(reverseString('hello world')).toBe('dlrow olleh');
+    });
+
+    test('reverse mix characters string', () => {
+        expect(reverseString('123Dogs!@# 4rE Gr34t')).toBe('t43rG Er4 #@!sgoD321');
+    });
+
+    test('message if not a string', () => {
+        expect(reverseString(123)).toBe('not a string');
+    });
+});
